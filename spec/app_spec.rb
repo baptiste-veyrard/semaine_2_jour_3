7ff629c79d1eea70e51f383d20c8ce5681fdacd3
Baptiste Veyrard <baptiste.veyrard@gmail.com>
2019-04-10 13:41:24 +0200

    pour leo

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -1,7 +1,7 @@
 require_relative '../lib/app'
 
 describe "the login_twitter method" do
-  it "should return client, and client is not nil" do
-    expect(login_twitter).not_to be_nil
+  it "should say hello to 5 random journalist from an handle" do
+    expect(dire_bonjour).not_to be_nil
   end
 end
